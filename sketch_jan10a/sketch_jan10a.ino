ce2ee77367533c39963f4f7b6c9e020a0d5c4acd
rnav <rnav@qlead.io>
2024-01-30 11:56:24 -0500

    Refactor keyCodes struct and add room lights control

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -16,52 +16,48 @@ const int RECV_PIN = A17;
 const int SEND_PIN = 4;
 const int LIGHT_SENSOR_PIN = 34;
 
-
-uint64_t datas[9] = {
-    0x1FE7887, // Power
-    0x1FE40BF, // Up
-    0x1FE20DF, // Left
-    0x1FE10EF, // Down
-    0x1FE609F, // Right
-    0x1FE50AF, // Clock
-    0x1FE30CF, // Temp
-    0x1FEF807, // Alarm
-    0x1FE708F  // Bright
+struct
+{
+  uint64_t power;
+  uint64_t up;
+  uint64_t left;
+  uint64_t down;
+  uint64_t right;
+  uint64_t clock;
+  uint64_t temp;
+  uint64_t alarm;
+  uint64_t bright;
+} keyCodes = {
+  .power = 0x1FE7887,
+  .up = 0x1FE40BF,
+  .left = 0x1FE20DF,
+  .down = 0x1FE10EF,
+  .right = 0x1FE609F,
+  .clock = 0x1FE50AF,
+  .temp = 0x1FE30CF,
+  .alarm = 0x1FEF807,
+  .bright = 0x1FE708F
 };
-
-// TODO!!! Move to separate file
-
 
 IRsend irsend(SEND_PIN);
 
-
-void setup() {
+void setup()
+{
   irsend.begin();
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFISSID, WIFIPW);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
     ESP.restart();
   }
 
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // Hostname defaults to esp3232-[MAC]
-  // ArduinoOTA.setHostname("myesp32");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
   ArduinoOTA
-    .onStart([]() {
+      .onStart([]()
+               {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH)
         type = "sketch";
@@ -69,28 +65,26 @@ void setup() {
         type = "filesystem";
 
       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
+      Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
       Serial.printf("Error[%u]: ", error);
       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
       else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+      else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
   ArduinoOTA.begin();
 
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
   time_t now = time(nullptr);
-  while (now < SECS_YR_2000) {
+  while (now < SECS_YR_2000)
+  {
     delay(100);
     now = time(nullptr);
   }
@@ -105,38 +99,49 @@ void setup() {
   TelnetStream.begin();
 }
 
+bool roomLightsDetected = true;
+const int LIGHT_SENSOR_THRESHOLD = 150;
 
-void loop() {
+void loop()
+{
   ArduinoOTA.handle();
+  handleTelnetRead();
 
-  switch (TelnetStream.read()) {
-    case 'R':
-    TelnetStream.stop();
-    delay(100);
-    ESP.restart();
-      break;
-    case 'C':
-      TelnetStream.println("bye bye");
-      TelnetStream.stop();
-      break;
+  int lightSensorReading = analogRead(LIGHT_SENSOR_PIN);
+  if (lightSensorReading > LIGHT_SENSOR_THRESHOLD && roomLightsDetected)
+  {
+    sendKeycode(keyCodes.power);
+    roomLightsDetected = false;
+    log("Room lights off");
+  }
+  else if (lightSensorReading < LIGHT_SENSOR_THRESHOLD && !roomLightsDetected)
+  {
+    sendKeycode(keyCodes.power);
+    roomLightsDetected = true;
+    log("Room lights on");
   }
 
-  static unsigned long next;
-  if (millis() - next > 1000) {
-    next = millis();
-    char reading[20];
-    sprintf(reading, "Light: %d", analogRead(LIGHT_SENSOR_PIN));
-    log(reading);
-  }
+  // static unsigned long next;
+  // if (millis() - next > 1000) {
+  // next = millis();
+  // char reading[20];
+  // sprintf(reading, "Light: %d", analogRead(LIGHT_SENSOR_PIN));
+  // log(reading);
+  // }
 
   // for (int i = 0; i < 9; i++) {
-  //   irsend.sendNEC(datas[i], 32, 2);      
+  //   irsend.sendNEC(datas[i], 32, 2);
   //   // delay(800);
   // }
 }
 
+void sendKeycode(uint64_t code)
+{
+  irsend.sendNEC(code, 32, 2);
+}
 
-void log(String msg) {
+void log(String msg)
+{
   static int i = 0;
 
   char timeStr[20];
@@ -149,3 +154,18 @@ void log(String msg) {
   TelnetStream.println(msg);
 }
 
+void handleTelnetRead()
+{
+  switch (TelnetStream.read())
+  {
+  case 'R':
+    TelnetStream.stop();
+    delay(100);
+    ESP.restart();
+    break;
+  case 'C':
+    TelnetStream.println("bye bye");
+    TelnetStream.stop();
+    break;
+  }
+}
