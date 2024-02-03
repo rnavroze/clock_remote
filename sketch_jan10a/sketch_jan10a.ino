2e564c945ea0c807bc5c1fc1858787e06848eb2b
rnav <rnav@qlead.io>
2024-02-03 17:00:09 -0500

    Add sunrise sunset logic.

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -7,15 +7,38 @@
 #include <ArduinoOTA.h>
 #include <TimeLib.h>
 #include <TelnetStream.h>
-// #include <sunset.h>
+#include <sunset.h>
+
+/****
+ * In the daytime, the clock will be set to the lowest brightness if the light sensor reads a value less than DIM_THRESHOLD_DAY.
+ * If the light sensor reads a value greater than BRIGHT_THRESHOLD_DAY, the clock will be set to the highest brightness.
+ * If the light sensor reads a value between DIM_THRESHOLD_DAY and BRIGHT_THRESHOLD_DAY, the clock will be set to the middle brightness.
+ * At night, the clock will be turned off if the light sensor reads a value less than OFF_THRESHOLD_NIGHT, and turned on otherwise.
+ *
+ * The ESP32 does not know the current state of the clock, so the clock is expected to be in this state when the ESP is turned on:
+ *   Brightness: Maximum
+ *   Power: Off
+ ****/
+
+// Thresholds
+#define OFF_THRESHOLD_NIGHT 50
+#define DIM_THRESHOLD_DAY 800
+#define BRIGHT_THRESHOLD_DAY 1200
+
+// Timezone
+#define GMTOFFSET_SEC -18000 // UTC-5
+#define DAYLIGHTOFFSET_SEC 3600
+
+// Pins
+#define RECV_PIN 27
+#define SEND_PIN 4
+#define LIGHT_SENSOR_PIN 34
+
+// Ensure WiFi details and location are configured in secrets.h
+// Define the following: WIFISSID, WIFIPW, LATITUDE, LONGITUDE
 #include "secrets.h"
 
-const long gmtOffset_sec = -18000; // UTC-5
-const int daylightOffset_sec = 3600;
-
-const int RECV_PIN = A17;
-const int SEND_PIN = 4;
-const int LIGHT_SENSOR_PIN = 34;
+///////// End of Configuration /////////
 
 struct
 {
@@ -40,12 +63,19 @@ struct
     .bright = 0x1FE708F};
 
 IRsend irsend(SEND_PIN);
+SunSet sun;
+int timezone;
 
 void setup()
 {
+    // Set up IRSend
     irsend.begin();
+
+    // Set up serial
     Serial.begin(115200);
     Serial.println("Booting");
+
+    // Connect to WiFi
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFISSID, WIFIPW);
     while (WiFi.waitForConnectResult() != WL_CONNECTED)
@@ -55,6 +85,7 @@ void setup()
         ESP.restart();
     }
 
+    // Set up ArduinoOTA
     ArduinoOTA
         .onStart([]()
                  {
@@ -81,7 +112,8 @@ void setup()
 
     ArduinoOTA.begin();
 
-    configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
+    // Set up NTP
+    configTime(GMTOFFSET_SEC, DAYLIGHTOFFSET_SEC, "pool.ntp.org");
     time_t now = time(nullptr);
     while (now < SECS_YR_2000)
     {
@@ -90,6 +122,19 @@ void setup()
     }
     setTime(now);
 
+    // Set up adjusted timezone
+    timezone = GMTOFFSET_SEC / 3600;
+    struct tm timeinfo;
+    getLocalTime(&timeinfo);
+    if (timeinfo.tm_isdst)
+    {
+        timezone++;
+    }
+
+    // Set up SunSet
+    sun.setPosition(LATITUDE, LONGITUDE, timezone);
+
+    // Print IP Address
     IPAddress ip = WiFi.localIP();
     Serial.println();
     Serial.println("Connected to WiFi network.");
@@ -100,39 +145,127 @@ void setup()
 }
 
 bool roomLightsDetected = true;
-const int LIGHT_SENSOR_THRESHOLD = 150;
+bool isPoweredOn = false;
+int brightness = 3; // 0: auto (unused), 1: low, 2: medium, 3: high
 
 void loop()
 {
+    // Handle OTA and Telnet
     ArduinoOTA.handle();
     handleTelnetRead();
 
+    // Set SunSet parameters
+    sun.setCurrentDate(year(), month(), day());
+    sun.setTZOffset(timezone);
+    double sunrise = sun.calcSunrise();
+    double sunset = sun.calcSunset();
+
+    // sunrise/sunset are in minutes past midnight, so we need to get ours too
+    double minsPastMidnight = hour() * 60 + minute() + GMTOFFSET_SEC / 60;
+
+    // Logging
+    static unsigned long next;
+    if (millis() - next > 1000)
+    {
+        next = millis();
+        log("Sunrise: " + String(sunrise));
+        log("Sunset: " + String(sunset));
+        log("Mins past midnight: " + String(minsPastMidnight));
+    }
+
     int lightSensorReading = analogRead(LIGHT_SENSOR_PIN);
-    if (lightSensorReading > LIGHT_SENSOR_THRESHOLD && roomLightsDetected)
+    // Different logic for day and night
+    if (minsPastMidnight > sunrise && minsPastMidnight < sunset)
     {
-        sendKeycode(keyCodes.power);
-        roomLightsDetected = false;
-        log("Room lights off");
+        if (!isPoweredOn) {
+            sendKeycode(keyCodes.power);
+            isPoweredOn = true;
+            log("Day / Powered on");
+        }
+        
+        // Day
+        if (lightSensorReading < DIM_THRESHOLD_DAY)
+        {
+            if (brightness != 1)
+            {
+
+                setBrightness(1);
+                log("Day / Brightness low");
+            }
+        }
+        else if (lightSensorReading > BRIGHT_THRESHOLD_DAY)
+        {
+            if (brightness != 3)
+            {
+                setBrightness(3);
+                log("Day / Brightness high");
+            }
+        }
+        else
+        {
+            if (brightness != 2)
+            {
+                setBrightness(2);
+                log("Day / Brightness medium");
+            }
+        }
     }
-    else if (lightSensorReading < LIGHT_SENSOR_THRESHOLD && !roomLightsDetected)
+    else
     {
-        sendKeycode(keyCodes.power);
-        roomLightsDetected = true;
-        log("Room lights on");
+        // Night
+        if (lightSensorReading > OFF_THRESHOLD_NIGHT && !isPoweredOn)
+        {
+            sendKeycode(keyCodes.power);
+            isPoweredOn = true;
+            log("Night / Powered on");
+        }
+        else if (lightSensorReading < OFF_THRESHOLD_NIGHT && isPoweredOn)
+        {
+            sendKeycode(keyCodes.power);
+            isPoweredOn = false;
+            log("Night / Powered off");
+        }
     }
+
+    // int lightSensorReading = analogRead(LIGHT_SENSOR_PIN);
+    // if (lightSensorReading > OFF_THRESHOLD_NIGHT && roomLightsDetected)
+    // {
+    //     sendKeycode(keyCodes.power);
+    //     roomLightsDetected = false;
+    //     log("Room lights off");
+    // }
+    // else if (lightSensorReading < OFF_THRESHOLD_NIGHT && !roomLightsDetected)
+    // {
+    //     sendKeycode(keyCodes.power);
+    //     roomLightsDetected = true;
+    //     log("Room lights on");
+    // }
 
     // static unsigned long next;
-    // if (millis() - next > 1000) {
-    // next = millis();
-    // char reading[20];
-    // sprintf(reading, "Light: %d", analogRead(LIGHT_SENSOR_PIN));
-    // log(reading);
+    // if (millis() - next > 1000)
+    // {
+    //     struct tm timeinfo;
+    //     getLocalTime(&timeinfo);
+    //     log("Is DST? " + String(timeinfo.tm_isdst));
+    //     next = millis();
+    //     char reading[20];
+    //     sprintf(reading, "Light: %d", analogRead(LIGHT_SENSOR_PIN));
+    //     log(reading);
     // }
+}
 
-    // for (int i = 0; i < 9; i++) {
-    //   irsend.sendNEC(datas[i], 32, 2);
-    //   // delay(800);
-    // }
+void setBrightness(int requestedBrightness)
+{
+    while (brightness != requestedBrightness)
+    {
+        brightness--;
+        if (brightness < 0)
+        {
+            brightness = 3;
+        }
+        sendKeycode(keyCodes.bright);
+        delay(250);
+    }
 }
 
 void sendKeycode(uint64_t code)
